7b53e12031c0c2fe46cf9990f26f2185fff16bb1
David Witt <david@davidwitt.com>
2018-05-20 07:25:07 -0400

    changed ratedSpeed to a Measurement<type>

--- Elevator/Data Models/Elevator.swift
+++ Elevator/Data Models/Elevator.swift
@@ -11,21 +11,37 @@ import Foundation
 let conversionFactorFpmToMps: Double = 0.00508
 
 class Elevator {
-  var ratedSpeed: MyMeasurement
+  var ratedSpeed: Measurement<Unit>
   var capacity: Double
   var governorSpeedReducingSwitch: Bool
   var staticControl: Bool
   var governorTrippingSpeed: MyMeasurement
 
   
-  init(ratedSpeed: MyMeasurement, capacity: Double, governorSpeedReducingSwitch: Bool, staticControl: Bool) {
+  init(ratedSpeed: Measurement<Unit>, capacity: Double, governorSpeedReducingSwitch: Bool, staticControl: Bool) {
     self.ratedSpeed = ratedSpeed
     self.capacity = capacity
     self.governorSpeedReducingSwitch = governorSpeedReducingSwitch
     self.staticControl = staticControl
     
     // self.governorTrippingSpeed = Measurement(value: 0.0, unit: ratedSpeed.unit)
-    governorTrippingSpeed = MyMeasurement(of: .speed, value: 0, units: ratedSpeed.units)
+    
+    // TODO: - Temporary fix for units
+    //         Until the governor units are changed
+    var governorUnits: UnitSystem
+    
+    let ratedSpeedUnit = ratedSpeed.unit
+    switch ratedSpeedUnit {
+    case UnitSpeed.metersPerSecond:
+      governorUnits = UnitSystem.metric
+    case UnitSpeed.feetPerMinute:
+      governorUnits = UnitSystem.imperial
+    default:
+      governorUnits = UnitSystem.metric
+    }
+    // MARK: -
+    
+    governorTrippingSpeed = MyMeasurement(of: .speed, value: 0, units: governorUnits)
   }
   
   
