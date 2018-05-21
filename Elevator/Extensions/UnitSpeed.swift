c91717f459eca36c06e8a5353e59f72da5e7b050
David Witt <david@davidwitt.com>
2018-05-21 11:40:14 -0400

    File missed in last commit

--- Elevator/Extensions/UnitSpeed.swift
+++ Elevator/Extensions/UnitSpeed.swift
@@ -11,23 +11,3 @@ import Foundation
 extension UnitSpeed {
   static let feetPerMinute =  UnitSpeed(symbol: "fpm", converter: UnitConverterLinear(coefficient: 304.8/60000.0))
 }
-
-// TODO: - Extension only required while refactoring for Foundation Measurement Class
-
-extension UnitSpeed {
-  var unitSystem: UnitSystem {
-    get {
-      var system: UnitSystem
-      
-      switch self.symbol {
-      case "m/s":
-        system = UnitSystem.metric
-      case "fpm":
-        system = UnitSystem.imperial
-      default:
-        system = UnitSystem.metric
-      }
-      return system
-    }
-  }
-}
